cf31f0755bcb54381640308163d073655fc5c654
Roger M <rsmoraldo2@gmail.com>
2017-02-19 10:34:27 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function addTwo(){
+var first = parseInt(document.getElementById('oneFinal').value);
+var second = parseInt(document.getElementById('twoFinal').value);
+
+console.log(first + second);
+}
+
+
+function subTwo(){
+var first = parseInt(document.getElementById('oneFinal').value);
+var second = parseInt(document.getElementById('twoFinal').value);
+
+console.log(first - second);
+}
+
+function mulTwo(){
+var first = parseInt(document.getElementById('oneFinal').value);
+var second = parseInt(document.getElementById('twoFinal').value);
+
+console.log(first * second);
+}
+
+function diveTwo(){
+var first = parseInt(document.getElementById('oneFinal').value);
+var second = parseInt(document.getElementById('twoFinal').value);
+
+console.log(first / second);
+}
